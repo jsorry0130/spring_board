950d3bd11c62d0f880c644c5b7301c21a5c02ba9
jsorry0130 <jsorry0130@naver.com>
2021-02-03 16:05:52 +0900

    board

--- board/src/main/java/com/board/dao/BoardDAO.java
+++ board/src/main/java/com/board/dao/BoardDAO.java
@@ -21,6 +21,9 @@ public interface BoardDAO {
 	public List listPage(int displayPost, int postNum) throws Exception;
 	
 	// 게시물 목록 + 페이징 + 검색
-	 public List<BoardVO> listPageSearch(
-			 int displayPost, int postNum, String searchType, String keyword) throws Exception;
+	public List<BoardVO> listPageSearch(
+			int displayPost, int postNum, String searchType, String keyword) throws Exception;
+	 
+	// 게시물 총 갯수 + 검색 적용
+	public int searchCount(String searchType, String keyword) throws Exception;
 }
